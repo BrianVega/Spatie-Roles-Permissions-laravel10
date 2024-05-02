fc0a264da549e48c40f45557cc532695b415689e
Brian Vega <brianvega01@outlook.es>
2024-05-02 13:15:57 -0600

    First commit

---
+++ resources/views/role-permission/permission/index.blade.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Permisos
+                    <a href="{{ url('permissions/create') }}" class="btn btn-primary float-end">Añadir permiso</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
